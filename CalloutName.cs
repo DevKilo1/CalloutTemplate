4700c53af0798e1bc9e389030e6eb3539390f871
Kilo <37485242+DevKilo1@users.noreply.github.com>
2024-05-01 16:57:37 -0400

    Initial commit

---
+++ CalloutName.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using FivePD.API;
+using Kilo.Commons.Utils;
+
+namespace CalloutTemplate
+{
+    public class CalloutName : Callout
+    {
+        public CalloutName()
+        {
+            ShortName = "CalloutName";
+            CalloutDescription = "";
+            StartDistance = 120f;
+            ResponseCode = 3;
+        }
+
+        public async override Task OnAccept()
+        {
+            
+        }
+        
+        public override void OnStart(Ped closest)
+        {
+        }
+
+        public override void OnCancelBefore()
+        {
+           
+        }
+    }
+}
